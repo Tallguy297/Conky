b5c992be38115c776f9fe6912b698d28b94c56f3
Alexander Gent <49935683+Tallguy297@users.noreply.github.com>
2020-07-08 09:24:31 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@
 apt-get -y install conky conky-all
 LOGGEDINUSER=$(users)
 cp -v -f files/conky.conf /home/$LOGGEDINUSER/.conkyrc
-chmod -v 777 -R /home/$LOGGEDINUSER/.conkyrc
+cp -v -f Conky.desktop /home/$LOGGEDINUSER/.config/autostart
+chmod -v 777 -R /home/$LOGGEDINUSER

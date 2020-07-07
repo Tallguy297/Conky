fe8a7187a34ec57d6a8ce626b48da91074b6c55c
Alexander Gent <49935683+Tallguy297@users.noreply.github.com>
2020-07-08 09:26:08 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ LOGGEDINUSER=$(users)
 cp -v -f files/conky.conf /home/$LOGGEDINUSER/.conkyrc
 cp -v -f Conky.desktop /home/$LOGGEDINUSER/.config/autostart
 chmod -v 777 -R /home/$LOGGEDINUSER
+shutdown -r now

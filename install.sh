ff2233e759703fe41a7d7612f7b2e44a369f1ecc
Alexander Gent <49935683+Tallguy297@users.noreply.github.com>
2020-12-20 02:39:27 +1100

    Delete install.sh

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-apt-get -y install conky conky-all
-LOGGEDINUSER=$(users)
-cp -v -f files/conky.conf /home/$LOGGEDINUSER/.conkyrc
-cp -v -f Conky.desktop /home/$LOGGEDINUSER/.config/autostart
-chmod -v 777 -R /home/$LOGGEDINUSER
-shutdown -r now
